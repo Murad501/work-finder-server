cdb8ba3d6c68045b8f0f6f8cb2f7eedf290701e7
Murad501 <mm845635@gmail.com>
2023-02-18 16:38:52 +0600

    post, applying api created

--- index.js
+++ index.js
@@ -1,36 +1,73 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
-
-app.get('/', (req, res)=>{
-    res.send('data is coming soon')
-})
-
+app.get("/", (req, res) => {
+  res.send("data is coming soon");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@user1.istzhai.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 console.log(uri);
-const run = async() => {
-    try{
-        const userCollection = client.db('Work-Finder').collection('users')
+const run = async () => {
+  try {
+    const usersCollection = client.db("Work-Finder").collection("users");
+    const companiesCollection = client.db('Work-Finder').collection('companies')
+    const postsCollection = client.db("Work-Finder").collection("posts");
+    const applyingCollection = client.db('Work-finder').collection('applying')
 
-    }
-    catch{
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const email = user.email;
+      const query = { email: email };
+      const addedUser = await usersCollection.findOne(query);
+      const addedCompany = await companiesCollection.findOne(query)
+      if (addedUser || addedCompany) {
+        return res.send({ acknowledged: true });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-    }
-}
+    app.post("/company", async (req, res) => {
+      const company = req.body;
+      const result = await companiesCollection.insertOne(company);
+      res.send(result);
+    });
 
-run().catch(err => console.log(err))
+    app.get('/posts', async(req, res) => {
+        const query = {}
+        const result = await postsCollection.find(query).toArray()
+        res.send(result)
+    })
 
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postsCollection.insertOne(post);
+      res.send(result);
+    });
 
-app.listen(port, ()=>{
-    console.log('server is running on port', port)
-})
+    app.post('/applying', async(req, res)=>{
+        const bookedJob = req.body
+        const result = await applyingCollection.insertOne(bookedJob)
+        res.send(result)
+    })
+  } catch {}
+};
+
+run().catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});
